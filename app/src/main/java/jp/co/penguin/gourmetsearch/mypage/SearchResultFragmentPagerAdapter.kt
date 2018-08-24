0592a81c67d986f03edee4df4892dd5e0493cb85
takayuki-hara <takayuki.hara.dev@gmail.com>
2018-08-24 18:54:08 +0900

    SearchResultMapFragmentを追加

--- app/src/main/java/jp/co/penguin/gourmetsearch/mypage/SearchResultFragmentPagerAdapter.kt
+++ app/src/main/java/jp/co/penguin/gourmetsearch/mypage/SearchResultFragmentPagerAdapter.kt
@@ -4,12 +4,13 @@ import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
 import jp.co.penguin.gourmetsearch.search.SearchResultListFragment
+import jp.co.penguin.gourmetsearch.search.SearchResultMapFragment
 
 class SearchResultFragmentPagerAdapter(fm: FragmentManager?) : FragmentPagerAdapter(fm) {
     override fun getItem(position: Int): Fragment {
         when (position) {
             0 -> return SearchResultListFragment()
-            1 -> return SearchResultListFragment()
+            1 -> return SearchResultMapFragment()
             else -> return SearchResultListFragment()
         }
     }
