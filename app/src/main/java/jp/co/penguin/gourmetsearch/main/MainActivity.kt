282d5b5ab37429a36436867f72374a3467bfbfae
takayuki-hara <takayuki.hara.dev@gmail.com>
2018-08-24 12:57:05 +0900

    InformationのFragmentを追加

--- app/src/main/java/jp/co/penguin/gourmetsearch/main/MainActivity.kt
+++ app/src/main/java/jp/co/penguin/gourmetsearch/main/MainActivity.kt
@@ -7,6 +7,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 import android.content.Intent
 import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 import jp.co.penguin.gourmetsearch.R
+import jp.co.penguin.gourmetsearch.information.InformationFragment
 import jp.co.penguin.gourmetsearch.mypage.MypageFragment
 import jp.co.penguin.gourmetsearch.search.SearchFragment
 
@@ -29,7 +30,7 @@ class MainActivity : AppCompatActivity() {
             }
             R.id.navigation_notifications -> {
                 message.setText(R.string.title_notifications)
-                val fragment = SearchFragment.newInstance(param1 = "", param2 = "")
+                val fragment = InformationFragment.newInstance(param1 = "", param2 = "")
                 supportFragmentManager.beginTransaction().replace(R.id.contentFrame, fragment).commit()
                 return@OnNavigationItemSelectedListener true
             }
