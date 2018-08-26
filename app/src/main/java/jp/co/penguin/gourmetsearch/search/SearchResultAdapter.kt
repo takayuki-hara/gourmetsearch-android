b0a4de3ff04ca8c55c543dd6e1dcfca8b796de72
takayuki-hara <takayuki.hara.dev@gmail.com>
2018-08-26 22:54:27 +0900

    検索結果のリストのレイアウトとデータの設定処理を追加

--- app/src/main/java/jp/co/penguin/gourmetsearch/search/SearchResultAdapter.kt
+++ app/src/main/java/jp/co/penguin/gourmetsearch/search/SearchResultAdapter.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
 import jp.co.penguin.gourmetsearch.R
 import jp.co.penguin.gourmetsearch.data.entity.Shop
 
@@ -18,7 +20,13 @@ class SearchResultAdapter(context: FragmentActivity?) : ArrayAdapter<Shop>(conte
             val root = this.inflater.inflate(R.layout.item_shop, parent, false)!!
 
             val vh = ViewHolder()
-            vh.nameText = root.findViewById(R.id.text_name)
+            vh.genreText = root.findViewById(R.id.genreText)
+            vh.nameText = root.findViewById(R.id.nameText)
+            vh.shopImage = root.findViewById(R.id.shopImage)
+            vh.budgetText = root.findViewById(R.id.budgetText)
+            vh.accessText = root.findViewById(R.id.accessText)
+            vh.openText = root.findViewById(R.id.openText)
+            vh.memoText = root.findViewById(R.id.memoText)
 
             root.tag = vh
 
@@ -30,14 +38,24 @@ class SearchResultAdapter(context: FragmentActivity?) : ArrayAdapter<Shop>(conte
         val shop = getItem(position)
 
         val vh = view.tag as ViewHolder
+        vh.accessText.text = shop.genre?.name
         vh.nameText.text = shop.name
-
+        Glide.with(view).load(shop.logoImage).into(vh.shopImage)
+        vh.budgetText.text = shop.budget?.name
+        vh.accessText.text = shop.mobileAccess
+        vh.openText.text = shop.open
+        vh.memoText.text = shop.shopDetailMemo
 
         return view
     }
 
     private class ViewHolder {
+        lateinit var genreText: TextView
         lateinit var nameText: TextView
-
+        lateinit var shopImage: ImageView
+        lateinit var budgetText: TextView
+        lateinit var accessText: TextView
+        lateinit var openText: TextView
+        lateinit var memoText: TextView
     }
 }
